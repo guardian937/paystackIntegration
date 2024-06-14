8aab8cad044efaf996b7a984d0697312c2b9ab75
guardian937 <mgodwin239@gmail.com>
2024-06-14 11:40:19 +0300

    Update payment.js

--- js/payment.js
+++ js/payment.js
@@ -4,10 +4,10 @@ function payWithPaystack(e) {
   e.preventDefault();
 
   let handler = PaystackPop.setup({
-    key: '<Replace with your Api Key>', // Replace with your public key
+    key: '<>', // Replace with your public key
     email: document.getElementById("email-address").value,
     amount: document.getElementById("amount").value * 100,
-    currency: 'GHS',
+    currency: 'GHS',pk_live_003efcbfa9875f4b3dd8e6f9b9ae681d554f7aac
     ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
     // label: "Optional string that replaces customer email"
     onClose: function(){
